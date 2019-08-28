c25094819ece6482346bca45773ca824e75e45f6
gymboss123 <53601202+gymboss123@users.noreply.github.com>
2019-08-28 21:42:42 +0700

    Update function.php

--- function.php
+++ function.php
@@ -10,7 +10,7 @@ $header[] = "X-AppVersion: 3.16.1";
 $header[] = "X-UniqueId: 106605982657".mt_rand(1000,9999);
 $header[] = "Connection: keep-alive";    
 $header[] = "X-User-Locale: en_ID";
-$header[] = "X-Location: -6.933133,110.202279";
+$header[] = "X-Location: -7.613805,110.633676";
 $header[] = "X-Location-Accuracy: 3.0";
 if ($pin):
 $header[] = "pin: $pin";    
