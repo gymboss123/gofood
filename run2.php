9091d9037e9d7cdf5fd88792544c97f8448399c2
gymboss123 <53601202+gymboss123@users.noreply.github.com>
2019-09-01 02:53:41 +0700

    Update run2.php

--- run2.php
+++ run2.php
@@ -77,7 +77,7 @@ function veriflogin($otp, $token)
 	}
 function claim($token)
 	{
-	$data = '{"promo_code":"GOFOODKERUPUK08"}';
+	$data = '{"promo_code":"GOFOODSATE15"}';
 	$claim = request("/go-promotions/v1/promotions/enrollments", $token, $data);
 	if ($claim['success'] == 1)
 		{
@@ -90,7 +90,7 @@ function claim($token)
 	}
 function claim1($token)
 	{
-	$data1 = '{"promo_code":"GOJEK17"}';
+	$data1 = '{"promo_code":"GOFOODSATE08"}';
 	$claim1 = request("/go-promotions/v1/promotions/enrollments", $token, $data1);
 	if ($claim1['success'] == 1)
 		{
@@ -103,7 +103,7 @@ function claim1($token)
 	}
 function claim2($token)
 	{
-	$data2 = '{"promo_code":"GOLIFEBARU08"}';
+	$data2 = '{"promo_code":"Gofoodkerupuk07"}';
 	$claim2 = request("/go-promotions/v1/promotions/enrollments", $token, $data2);
 	if ($claim2['success'] == 1)
 		{
