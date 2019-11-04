d6dd5f1f96bb496b2ee15febecedc0fb280502db
gymboss123 <53601202+gymboss123@users.noreply.github.com>
2019-11-05 06:12:08 +0700

    Update run1.php

--- run1.php
+++ run1.php
@@ -103,7 +103,7 @@ function claim1($token)
 	}
 function claim2($token)
 	{
-	$data2 = '{"promo_code":"GOPAYDIIKEA"}';
+	$data2 = '{"promo_code":"AYOCOBAGOJEK"}';
 	$claim2 = request("/go-promotions/v1/promotions/enSCANPRAMUKArollments", $token, $data2);
 	if ($claim2['success'] == 1)
 		{
